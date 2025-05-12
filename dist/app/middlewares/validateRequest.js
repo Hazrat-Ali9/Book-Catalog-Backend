6da207bc0fa3ce0a64420a7a5fcab8770c76d013
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-12 20:02:27 +0600

    Validate Request  🏥

--- dist/app/middlewares/validateRequest.js
+++ dist/app/middlewares/validateRequest.js
@@ -8,6 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// Validate Request
 Object.defineProperty(exports, "__esModule", { value: true });
 const validateRequest = (schema) => (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
