4bfbb2134b70ca76734b737caea435e640c1db7d
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-04 15:56:16 +0600

    profile route 🏀

--- src/app/modules/profile/profile.route.ts
+++ src/app/modules/profile/profile.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import { ProfileController } from './profile.controller';
-
+//profile route
 const router = express.Router();
 router.get(
   '/',
