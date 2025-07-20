0b50123582095cd0ab9896d60c09690a4ad3cdac
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-20 16:21:23 +0600

    config index  ts :bulb:

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import dotenv from 'dotenv';
 import path from 'path';
-
+// config index
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
